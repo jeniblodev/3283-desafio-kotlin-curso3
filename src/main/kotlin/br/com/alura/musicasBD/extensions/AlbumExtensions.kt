cb833dbe78e5e74fe048a5a92085a90e9b491a0d
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-29 19:00:57 -0300

    Resolução parte #2

--- src/main/kotlin/br/com/alura/musicasBD/extensions/AlbumExtensions.kt
+++ src/main/kotlin/br/com/alura/musicasBD/extensions/AlbumExtensions.kt
@@ -1,12 +1,12 @@
 package br.com.alura.musicasBD.extensions
 
-//import br.com.alura.musicasBD.dados.AlbumEntity
-//import br.com.alura.musicasBD.modelos.Album
-//
-//fun Album.toEntity(): AlbumEntity {
-//    return AlbumEntity(this.titulo, this.banda.toEntity(),this.id)
-//}
-//
-//fun AlbumEntity.toModel(): Album {
-//    return Album(this.titulo, this.banda.toModel(), this.id)
-//}
+import br.com.alura.musicasBD.dados.AlbumEntity
+import br.com.alura.musicasBD.modelos.Album
+
+fun Album.toEntity(): AlbumEntity {
+    return AlbumEntity(this.titulo, this.banda.toEntity(),this.id)
+}
+
+fun AlbumEntity.toModel(): Album {
+    return Album(this.titulo, this.banda.toModel(), this.id)
+}
